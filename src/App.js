12cd819c482263dcc21af75d79cef3c8ad44b95d
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-06-30 13:37:51 -0600

    setting up homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleClick = (e) => {
+    (e.target.style.width === '50px' || e.target.style.width === '')
+        ? e.target.style.width = 'fit-content' 
+        : e.target.style.width = '50px';
+    const db_json = e.target.childNodes[1]
+    if (db_json.style.display === "block") {
+      db_json.style.display = "none";
+    } else {
+      db_json.style.display = "block";
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="slide" onClick={handleClick}>
+        Access db.json
+        <div id="dbjson">
+          <img src="https://i.imgur.com/N14u1t5.png" alt="source: imgur.com" />
+        </div>
+      </div>
+      <div id="app-main-content">
+        <div className="header">
+          <h1>Reactception</h1>
+          <h3>A Componenet Within a Component</h3>
+        </div>
+        <div className="code-snapshots">
+          <img src="https://i.imgur.com/gZrjyVO.png" alt="source: imgur.com" />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
